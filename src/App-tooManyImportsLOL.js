c244af0fbc34b9ffdda0a193161e8fac842f01d8
James Weldy <jamesweldy@Jims-MacBook-Pro.local>
2022-01-18 20:01:46 -0600

    fresh new auth included now

--- src/App-tooManyImportsLOL.js
+++ src/App-tooManyImportsLOL.js
@@ -2,15 +2,16 @@ import { Amplify } from 'aws-amplify';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hey there, {user.username}! We now have Auth!</h1>
+        <h1>Hello {user.username}</h1>
         <button onClick={signOut}>Sign out</button>
       </header>
     </div>
